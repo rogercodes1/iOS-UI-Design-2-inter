10dc6d6f32336d8fd84e0e4903be09c9601cc5b7
rogercodes1 <rogerperez.us@gmail.com>
2019-01-25 13:49:47 -0800

    added WebVC

---
+++ iOS-ui-design-intermediate/WebVC.swift
@@ -0,0 +1,30 @@
+//
+//  WebVC.swift
+//  iOS-ui-design-intermediate
+//
+//  Created by Roger Perez on 1/25/19.
+//  Copyright © 2019 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class WebVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
