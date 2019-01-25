5e3893ac60b656637bab905f7bcd56c8e3709657
rogercodes1 <rogerperez.us@gmail.com>
2019-01-25 15:38:08 -0800

    added tableVC and table view controller

---
+++ iOS-ui-design-intermediate/TableVC.swift
@@ -0,0 +1,40 @@
+//
+//  TableVC.swift
+//  iOS-ui-design-intermediate
+//
+//  Created by Roger Perez on 1/25/19.
+//  Copyright © 2019 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class TableVC: UIViewController, UITableViewDataSource {
+ 
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
