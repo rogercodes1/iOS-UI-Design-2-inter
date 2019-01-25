c38b52be57cfc2b1bf4325bb7a25d69ced8bd65f
rogercodes1 <rogerperez.us@gmail.com>
2019-01-25 11:52:19 -0800

    added SliderVC and progBar and sliderBar

---
+++ iOS-ui-design-intermediate/SliderVC.swift
@@ -0,0 +1,39 @@
+//
+//  SliderVC.swift
+//  iOS-ui-design-intermediate
+//
+//  Created by Roger Perez on 1/25/19.
+//  Copyright © 2019 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class SliderVC: UIViewController {
+
+    
+    @IBOutlet weak var progBar: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        progBar.progress = sender.value
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
