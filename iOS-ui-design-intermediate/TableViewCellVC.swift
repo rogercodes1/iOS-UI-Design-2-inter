f20804fe74a71eb4a5253f2a9b2d6c59007e69d5
rogercodes1 <rogerperez.us@gmail.com>
2019-01-28 08:41:54 -0800

    added tableViewCellVC

---
+++ iOS-ui-design-intermediate/TableViewCellVC.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellVC.swift
+//  iOS-ui-design-intermediate
+//
+//  Created by Roger Perez on 1/28/19.
+//  Copyright © 2019 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
